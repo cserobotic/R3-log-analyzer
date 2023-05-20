9e11881b5ba753d285a3ba54989c4ff9762d0775
Arya Parvizi <aryaparvizi98@gmail.com>
2023-05-20 22:18:00 +0330

    chore: Change default directory to this directory.

--- loganalyzer/PositionTracking.py
+++ loganalyzer/PositionTracking.py
@@ -1,17 +1,13 @@
-# if(filename[-3:]=='.gz'):
-#     print('filename=',myargs+'/'+filename[:-7])
-#     gunzip(myargs+'/*.gz')
-#     parser = Parser(myargs+'/'+filename[:-7])
-# else:    
+import sys
+from loganalyzer.Parser import *
+from loganalyzer.Game import *
+from loganalyzer.Analyzer import *
 
-print('file number:', ctr)
-
-print('filename= ',myargs+'/'+filename[:-4])
-parser = Parser(myargs+'/'+filename[:-4])
+parser = Parser(sys.argv[1])
 
 game = Game(parser)
-analyzer = Analyzer(game, myargs+'/'+filename[:-4])
+analyzer = Analyzer(game)
 analyzer.analyze()
 
 analyzer.draw_heatmap()
-analyzer.draw_heatmap(right_team=True)
+analyzer.draw_heatmap(right_team=True) 
